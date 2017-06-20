3bc592a1fea547f8f04e75f3b52be56316302b32
Ted John <ted@brambles.org>
2017-06-20 22:40:10 +0100

    Now x64 packages

--- install.bat
+++ install.bat
@@ -20,13 +20,13 @@ pushd %VCPKG%
 
   REM Install x64 libraries
   REM .\vcpkg install curl:x64-windows-static
-  REM .\vcpkg install freetype:x64-windows-static
-  REM .\vcpkg install libpng:x64-windows-static
-  REM .\vcpkg install sdl2:x64-windows-static
-  REM .\vcpkg install openssl:x64-windows-static
-  REM .\vcpkg install zlib:x64-windows-static
+  .\vcpkg install freetype:x64-windows-static
+  .\vcpkg install libpng:x64-windows-static
+  .\vcpkg install sdl2:x64-windows-static
+  .\vcpkg install openssl:x64-windows-static
+  .\vcpkg install zlib:x64-windows-static
   REM .\vcpkg install jansson:x64-windows-static
   REM .\vcpkg install libspeex:x64-windows-static
-  REM .\vcpkg install libzip:x64-windows-static
+  .\vcpkg install libzip:x64-windows-static
 
 popd
