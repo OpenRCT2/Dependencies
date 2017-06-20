4e66b80afcdd0d7ebf330f2c072451e3916041bc
Ted John <ted@brambles.org>
2017-06-20 22:39:38 +0100

    Again: start with x86 packages except curl

--- install.bat
+++ install.bat
@@ -8,7 +8,7 @@ pushd %VCPKG%
   git pull
 
   REM Install x86 libraries
-  .\vcpkg install curl:x86-windows-static
+  REM .\vcpkg install curl:x86-windows-static
   .\vcpkg install freetype:x86-windows-static
   .\vcpkg install libpng:x86-windows-static
   .\vcpkg install sdl2:x86-windows-static
@@ -19,14 +19,14 @@ pushd %VCPKG%
   .\vcpkg install libzip:x86-windows-static
 
   REM Install x64 libraries
-  .\vcpkg install curl:x64-windows-static
-  .\vcpkg install freetype:x64-windows-static
-  .\vcpkg install libpng:x64-windows-static
-  .\vcpkg install sdl2:x64-windows-static
-  .\vcpkg install openssl:x64-windows-static
-  .\vcpkg install zlib:x64-windows-static
+  REM .\vcpkg install curl:x64-windows-static
+  REM .\vcpkg install freetype:x64-windows-static
+  REM .\vcpkg install libpng:x64-windows-static
+  REM .\vcpkg install sdl2:x64-windows-static
+  REM .\vcpkg install openssl:x64-windows-static
+  REM .\vcpkg install zlib:x64-windows-static
   REM .\vcpkg install jansson:x64-windows-static
   REM .\vcpkg install libspeex:x64-windows-static
-  .\vcpkg install libzip:x64-windows-static
+  REM .\vcpkg install libzip:x64-windows-static
 
 popd
