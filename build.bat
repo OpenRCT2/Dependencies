1ac43d44b5bd3d290ff7bf5ab9cbb4dee9c98ee6
Ted John <ted@brambles.org>
2017-08-11 21:18:38 +0100

    Fix spacey paths

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 REM add linker to PATH
-env
 set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\bin\Host%1\%1;%PATH%
 
 set packages=%VCPKG%\installed\%1-windows-static
@@ -7,7 +6,7 @@ set artifacts=%CD%\artifacts
 set outlib=%artifacts%\openrct2-libs-vs2017-%1.lib
 
 REM aggregate libs
-mkdir %artifacts%
+mkdir "%artifacts%"
 pushd "%packages%\lib"
     lib /LTCG "/OUT:%outlib%" bz2.lib freetype.lib jansson.lib libcurl.lib libeay32.lib libpng16.lib libssh2.lib SDL2.lib ssleay32.lib zip.lib zlib.lib
 popd
