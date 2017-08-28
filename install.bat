50afe7d8a7cf1803d9a0bf795a68b58ebd96f68d
Ted John <ted@brambles.org>
2017-08-28 12:26:56 +0100

    Add speexdsp and sort packages

--- install.bat
+++ install.bat
@@ -7,26 +7,29 @@ pushd %VCPKG%
   )
   git pull
 
+  REM Uninstall out of date packages so they are updated
+  .\vcpkg remove --outdated
+
   REM Install x86 libraries
   .\vcpkg install curl:x86-windows-static
   .\vcpkg install freetype:x86-windows-static
-  .\vcpkg install libpng:x86-windows-static
-  .\vcpkg install sdl2:x86-windows-static
-  .\vcpkg install openssl:x86-windows-static
-  .\vcpkg install zlib:x86-windows-static
   .\vcpkg install jansson:x86-windows-static
-  REM .\vcpkg install libspeex:x86-windows-static
+  .\vcpkg install libpng:x86-windows-static
   .\vcpkg install libzip:x86-windows-static
+  .\vcpkg install openssl:x86-windows-static
+  .\vcpkg install sdl2:x86-windows-static
+  .\vcpkg install speexdsp:x86-windows-static
+  .\vcpkg install zlib:x86-windows-static
 
   REM Install x64 libraries
   .\vcpkg install curl:x64-windows-static
   .\vcpkg install freetype:x64-windows-static
-  .\vcpkg install libpng:x64-windows-static
-  .\vcpkg install sdl2:x64-windows-static
-  .\vcpkg install openssl:x64-windows-static
-  .\vcpkg install zlib:x64-windows-static
   .\vcpkg install jansson:x64-windows-static
-  REM .\vcpkg install libspeex:x64-windows-static
+  .\vcpkg install libpng:x64-windows-static
   .\vcpkg install libzip:x64-windows-static
+  .\vcpkg install openssl:x64-windows-static
+  .\vcpkg install sdl2:x64-windows-static
+  .\vcpkg install speexdsp:x64-windows-static
+  .\vcpkg install zlib:x64-windows-static
 
 popd
