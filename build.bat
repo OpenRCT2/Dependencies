3e80ba677d4176e900e47caed8e9fbad92242a78
Tobias Kohlbau <tobias@kohlbau.de>
2018-01-21 20:05:40 +0100

    bundle debug and release deps
    Signed-off-by: Tobias Kohlbau <tobias@kohlbau.de>

--- build.bat
+++ build.bat
@@ -1,12 +1,34 @@
 REM add linker to PATH
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\bin\Host%1\%1;%PATH%
+@echo off
 
-set packages=%VCPKG%\installed\%1-windows-static
+set cdir=%CD%
+
+for /f "usebackq tokens=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+if %1=="x64" (
+    set devEnv="-arch=x64 -host_arch=x64"
+)
+if %1=="x86" (
+    set devEnv="-arch=x86 -host_arch=x86"
+)
+
+if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+  call "%InstallDir%\Common7\Tools\vsdevcmd.bat" %devEnv% -no_logo
+)
+
+cd %cdir%
+
+set packages=C:\tools\vcpkg\installed\%1-windows-static
 set artifacts=%CD%\artifacts
-set outlib=%artifacts%\openrct2-libs-vs2017-%1.lib
+set outlib=%artifacts%\openrct2-libs-vs2017-%1
 
 REM aggregate libs
 mkdir "%artifacts%"
 pushd "%packages%\lib"
-    lib /LTCG "/OUT:%outlib%" bz2.lib discord-rpc.lib freetype.lib jansson.lib libcurl.lib libeay32.lib libpng16.lib libspeexdsp.lib libssh2.lib SDL2.lib ssleay32.lib zip.lib zlib.lib
+    lib /LTCG "/OUT:%outlib%.lib" bz2.lib discord-rpc.lib freetype.lib jansson.lib libcurl.lib libeay32.lib libpng16.lib libspeexdsp.lib libssh2.lib SDL2.lib ssleay32.lib zip.lib zlib.lib
+popd
+pushd "%packages%\debug\lib"
+    lib /LTCG "/OUT:%outlib%d.lib" bz2d.lib discord-rpc.lib freetyped.lib jansson_d.lib libcurl.lib libeay32.lib libpng16d.lib libspeexdsp.lib libssh2.lib SDL2d.lib ssleay32.lib zip.lib zlibd.lib
 popd
