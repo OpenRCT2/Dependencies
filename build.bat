432dd8885d7fb61bf3b1fec6dd3c6b0cc94ea216
Michał Janiszewski <janisozaur@gmail.com>
2018-03-02 22:14:36 +0100

    Correctly descend/ascend directories

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
+pushd vcpkg
+
 REM Install libraries
 .\vcpkg install curl:%TRIPLET% discord-rpc:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% openssl:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
 
 REM Export libraries
 .\vcpkg export curl:%TRIPLET% discord-rpc:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% openssl:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET% --zip --nuget
+
+popd
--- install.bat
+++ install.bat
@@ -2,9 +2,10 @@ REM Install upstream vcpkg
 
 git clone -q https://github.com/Microsoft/vcpkg.git
 robocopy /v /fp triplets vcpkg/triplets/
-cd vcpkg
+pushd vcpkg
 call .\bootstrap-vcpkg.bat
 git pull
 
 REM Uninstall out of date packages so they are updated
 .\vcpkg remove --outdated --recurse
+popd
