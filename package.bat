48755e0fe108ccbfe2f77981434495333d9758f4
Michał Janiszewski <janisozaur@gmail.com>
2018-03-03 00:02:37 +0100

    7z is unable to modify paths upon storage, fix by changing directory

--- package.bat
+++ package.bat
@@ -1,5 +1,9 @@
 set packages=vcpkg\installed\%TRIPLET%
 set outzip=openrct2-libs-%TRIPLET%.zip
 
+pushd %packages%
+
 REM create zip
-7z a -tzip -mx9 -mtc=off "%outZip%" "%packages%\*"
+7z a -tzip -mx9 -mtc=off "%outZip%" "*"
+
+popd
