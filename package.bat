560cbe0fdb4c87e179568926b9113868cc6ec8a4
Michał Janiszewski <janisozaur@gmail.com>
2018-03-09 14:26:33 +0100

    Remove 7z, add autodeploy

--- package.bat
+++ package.bat
@@ -1,10 +1,9 @@
 set packages=vcpkg\installed\%TRIPLET%
-set outarchive=openrct2-libs-%TRIPLET%
+set outarchive=openrct2-libs-v%version%-%TRIPLET%
 
 pushd %packages%
 
 REM create zip
 7z a -tzip -mx9 -mtc=off "%outarchive%.zip" "*"
-7z a -t7z -mx9 -mtc=off "%outarchive%.7z" "*"
 
 popd
