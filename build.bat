75207db684e8cd120bddbdb240c4e66c733ffe23
Ted John <ted.john@microfocus.com>
2020-09-03 10:31:16 +0100

    Add FriBidi to Windows dependencies

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 setlocal
 pushd vcpkg
-  set libraries=benchmark:%TRIPLET% breakpad:%TRIPLET% discord-rpc:%TRIPLET% duktape:%TRIPLET% freetype:%TRIPLET% libpng:%TRIPLET% libzip[core]:%TRIPLET% nlohmann-json:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
+  set libraries=benchmark:%TRIPLET% breakpad:%TRIPLET% discord-rpc:%TRIPLET% duktape:%TRIPLET% freetype:%TRIPLET% fribidi:%TRIPLET% libpng:%TRIPLET% libzip[core]:%TRIPLET% nlohmann-json:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
   .\vcpkg install %libraries%
   .\vcpkg export %libraries% --zip --nuget
 popd
