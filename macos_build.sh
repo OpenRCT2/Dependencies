923e9fef262ada1c22e2f0d15e5bb04047e77f18
LRFLEW <LRFLEW@aol.com>
2020-11-19 18:44:59 -0600

    Add duktape to macOS vcpkg build

--- macos_build.sh
+++ macos_build.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
+brew install libyaml
+sudo easy_install pyyaml
+
 git clone -q https://github.com/Microsoft/vcpkg.git
 vcpkg/bootstrap-vcpkg.sh
 
-TRIPLET=""
-LIBRARIES=""
-vcpkg/vcpkg install \
-  --overlay-triplets=. --triplet=x64-osx-openrct2 \
-  freetype libpng 'libzip[core]' nlohmann-json sdl2
+TRIPLET="--overlay-triplets=. --triplet=x64-osx-openrct2"
+LIBRARIES="duktape freetype libpng libzip[core] nlohmann-json sdl2"
+vcpkg/vcpkg install ${=TRIPLET} ${=LIBRARIES}  
