95bb338f15fa6c7f94386aa8d2e65dfdf86c5f6d
LRFLEW <LRFLEW@aol.com>
2020-11-25 21:48:32 -0600

    add speexdsp, openssl, and min version specification

--- macos_build.sh
+++ macos_build.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
-brew install libyaml
-sudo easy_install pyyaml
+easy_install --user pyyaml
 
-git clone -q https://github.com/Microsoft/vcpkg.git
+#git clone -q https://github.com/Microsoft/vcpkg.git
+git clone -q https://github.com/LRFLEW/vcpkg.git
+git -C vcpkg checkout openrct2-devel
+
 vcpkg/bootstrap-vcpkg.sh
 
 TRIPLET="--overlay-triplets=. --triplet=x64-osx-openrct2"
-LIBRARIES="duktape freetype libpng libzip[core] nlohmann-json sdl2"
+LIBRARIES="duktape freetype libpng libzip[core] nlohmann-json openssl sdl2 speexdsp"
 vcpkg/vcpkg install ${=TRIPLET} ${=LIBRARIES}  
--- x64-osx-openrct2.cmake
+++ x64-osx-openrct2.cmake
@@ -3,6 +3,9 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_DEPLOYMENT_TARGET "10.13")
+
+set(VCPKG_BUILD_TYPE release)
 
 #if(PORT STREQUAL "zlib" OR PORT STREQUAL "bzip2")
 #    set(CURRENT_PORT_DIR "../dummy-port")
