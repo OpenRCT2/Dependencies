a147f0b8ffdcfc88905457f1ba2771c925ad5cb1
adam-bloom <adamb529@gmail.com>
2021-01-30 17:09:15 -0700

    remove unused windows .bat scripts

--- install.bat
+++
@@ -1,6 +0,0 @@
-REM Install upstream vcpkg
-git clone -q https://github.com/Microsoft/vcpkg.git
-pushd vcpkg
-  git apply ..\0001-Patch-duktape.patch
-  call .\bootstrap-vcpkg.bat
-popd
--- package.bat
+++
@@ -1,5 +0,0 @@
-set packages=vcpkg\installed\%TRIPLET%
-set outarchive=openrct2-libs-v%version%-%TRIPLET%
-pushd %packages%
-  7z a -tzip -mx9 -mtc=off "%outarchive%.zip" "*"
-popd
