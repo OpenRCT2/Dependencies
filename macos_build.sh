900e9ee082a877903ce56d6d7dbea0f9a5f5c07c
adam-bloom <adamb529@gmail.com>
2021-08-08 10:40:34 -0600

    build x64 and arm64, merge together

--- macos_build.sh
+++ macos_build.sh
@@ -4,8 +4,7 @@ easy_install --user pyyaml
 
 git clone -q https://github.com/Microsoft/vcpkg.git
 
-
-if [ -z "$VCPKG_COMMIT_HASH" ]; then
+if [ -v VCPKG_COMMIT_HASH ]; then
   echo "Using pinned vcpkg commit: ${VCPKG_COMMIT_HASH}"
   pushd vcpkg
   git checkout -q $VCPKG_COMMIT_HASH
@@ -14,11 +13,22 @@ fi
 
 vcpkg/bootstrap-vcpkg.sh
 
-TRIPLET="--overlay-triplets=. --triplet=x64-osx-openrct2"
-LIBRARIES="duktape freetype libpng libzip[core] nlohmann-json openssl sdl2 speexdsp discord-rpc icu"
-vcpkg/vcpkg install ${=TRIPLET} ${=LIBRARIES}
+ARM_TRIPLET="--overlay-triplets=. --triplet=arm64-osx-openrct2"
+X64_TRIPLET="--overlay-triplets=. --triplet=x64-osx-openrct2"
+LIBRARIES="libpng freetype openssl icu duktape libzip[core] nlohmann-json sdl2 speexdsp discord-rpc"
+vcpkg/vcpkg install ${=ARM_TRIPLET} ${=LIBRARIES}
+vcpkg/vcpkg install ${=X64_TRIPLET} ${=LIBRARIES}
+
+rsync -ah vcpkg/installed/x64-osx-openrct2/* universal-osx-openrct2
+for lib in vcpkg/installed/x64-osx-openrct2/lib/*.dylib; do
+    if [ -f "$lib" ] && [ ! -L $lib ]; then
+    libname=$(basename "$lib")
+      echo "Creating universal (fat) $libname"
+      lipo -create "vcpkg/installed/x64-osx-openrct2/lib/$libname" "vcpkg/installed/arm64-osx-openrct2/lib/$libname" -output "universal-osx-openrct2/lib/$libname"
+    fi
+done
 
 (
-  cd vcpkg/installed/x64-osx-openrct2 &&
-  zip -rXy ../../../openrct2-libs-v${version}-x64-macos-dylibs.zip * -x '*/.*'
+  cd universal-osx-openrct2 &&
+  zip -rXy ../openrct2-libs-v${version}-universal-macos-dylibs.zip * -x '*/.*'
 )
