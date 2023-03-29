4a45bcad7e41c78376911bb8bafdc8a3c9068475
Michał Janiszewski <janisozaur+signed@gmail.com>
2023-03-29 23:56:45 +0200

    Test build of arm64 on macos

--- macos_build.sh
+++ macos_build.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # exit on error
-set -e
+set -ex
 
 easy_install --user pyyaml==5.4.1 
 
@@ -19,29 +19,5 @@ vcpkg/bootstrap-vcpkg.sh
 ARM_TRIPLET="--overlay-triplets=. --triplet=arm64-osx-openrct2"
 X64_TRIPLET="--overlay-triplets=. --triplet=x64-osx-openrct2"
 LIBRARIES="libpng freetype openssl icu libzip[core] nlohmann-json openal-soft sdl2 speexdsp discord-rpc gtest libflac libogg libvorbis"
-vcpkg/vcpkg install ${=ARM_TRIPLET} ${=LIBRARIES}
-vcpkg/vcpkg install ${=X64_TRIPLET} ${=LIBRARIES}
-
-rsync -ah vcpkg/installed/x64-osx-openrct2/* universal-osx-openrct2
-for lib in vcpkg/installed/x64-osx-openrct2/lib/*.dylib; do
-    if [ -f "$lib" ] && [ ! -L $lib ]; then
-      lib_filename=$(basename "$lib")
-      lib_name=$(echo $lib_filename | cut -d'.' -f 1)
-      echo "Creating universal (fat) $lib_name"
-      if [ "$lib_name" = "libzip" ]; then
-        # libzip embeds the full rpath in LC_RPATH
-        # they will be different for arm64 and x86_64
-        # this will cause issues, and is unnecessary
-        install_name_tool -delete_rpath `pwd`"/vcpkg/packages/${lib_name}_x64-osx-openrct2/lib" "vcpkg/installed/x64-osx-openrct2/lib/$lib_filename"
-        install_name_tool -delete_rpath `pwd`"/vcpkg/installed/x64-osx-openrct2/lib" "vcpkg/installed/x64-osx-openrct2/lib/$lib_filename"
-        install_name_tool -delete_rpath `pwd`"/vcpkg/packages/${lib_name}_arm64-osx-openrct2/lib" "vcpkg/installed/arm64-osx-openrct2/lib/$lib_filename"
-        install_name_tool -delete_rpath `pwd`"/vcpkg/installed/arm64-osx-openrct2/lib" "vcpkg/installed/arm64-osx-openrct2/lib/$lib_filename"
-      fi
-      lipo -create "vcpkg/installed/x64-osx-openrct2/lib/$lib_filename" "vcpkg/installed/arm64-osx-openrct2/lib/$lib_filename" -output "universal-osx-openrct2/lib/$lib_filename"
-    fi
-done
-
-(
-  cd universal-osx-openrct2 &&
-  zip -rXy ../openrct2-libs-v${version}-universal-macos-dylibs.zip * -x '*/.*'
-)
+vcpkg/vcpkg install ${=ARM_TRIPLET} "icu" || true
+cat /Users/runner/work/Dependencies/Dependencies/vcpkg/buildtrees/icu/make-build-fix-rpath-arm64-osx-openrct2-rel-err.log
