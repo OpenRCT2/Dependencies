8f563311eb4e46668a0b731feeba54e8f8114ed1
Michał Janiszewski <janisozaur+signed@gmail.com>
2023-03-30 11:49:46 +0200

    Test with amd64

--- macos_build.sh
+++ macos_build.sh
@@ -19,6 +19,6 @@ vcpkg/bootstrap-vcpkg.sh
 ARM_TRIPLET="--overlay-triplets=. --triplet=arm64-osx-openrct2"
 X64_TRIPLET="--overlay-triplets=. --triplet=x64-osx-openrct2"
 LIBRARIES="libpng freetype openssl icu[core,tools] libzip[core] nlohmann-json openal-soft sdl2 speexdsp discord-rpc gtest libflac libogg libvorbis"
-vcpkg/vcpkg install ${=ARM_TRIPLET} "icu[core,tools]" || true
+vcpkg/vcpkg install ${=X64_TRIPLET} "icu[core,tools]" || true
 find /Users/runner/work/Dependencies/Dependencies/vcpkg/ -name '*icu*.dylib'
 cat /Users/runner/work/Dependencies/Dependencies/vcpkg/buildtrees/icu/make-build-fix-rpath-arm64-osx-openrct2-rel-err.log
